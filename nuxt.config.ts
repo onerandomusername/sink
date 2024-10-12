ea2f99d3c68ecfbdb1c15ae789e560eb4ffb45e5
onerandomusername <me@arielle.codes>
2024-10-12 04:13:55 -0400

    feat: use custom 404 page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
     linkCacheTtl: 60,
     redirectWithQuery: false,
     homeURL: '',
+    notFoundURL: '',
     cfAccountId: '',
     cfApiToken: '',
     dataset: 'sink',
